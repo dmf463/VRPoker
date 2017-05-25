8502680019d6a8acd883b1e710761bc28a98a3fb
denniscarr <dcarr1@gmail.com>
2017-05-25 14:53:38 -0400

    Stopped the super wild rotation when I threw cards

---
+++ Assets/Scripts/PlayingCardScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayingCardScript : MonoBehaviour {
+
+    Vector3 myRotation;
+    public float torque;
+    Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        myRotation = transform.eulerAngles;
+        transform.rotation = Quaternion.Euler(0, myRotation.y, myRotation.z);
+
+    }
+}
