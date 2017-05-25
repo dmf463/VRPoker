60ec8fca7e0c6f2ab4c6ca8628d684a582113fb5
davidFrisch <dmf463@nyu.edu>
2017-05-25 15:39:35 -0400

    started on lerping the proper rotation when I throw
    probably should change it to make the torque in proportion to the velocity so that if I just place a card it doesn't rotate.

--- Assets/Scripts/PlayingCardScript.cs
+++ Assets/Scripts/PlayingCardScript.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayingCardScript : MonoBehaviour {
 
     Vector3 myRotation;
-    public float torque;
     Rigidbody rb;
+    public float torque;
+    public float duration;
+    bool startLerping;
+    float elapsedTime;
 
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         rb = GetComponent<Rigidbody>();
 
@@ -19,8 +22,27 @@ public class PlayingCardScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        Debug.Log("torque is " + torque);
         myRotation = transform.eulerAngles;
         transform.rotation = Quaternion.Euler(0, myRotation.y, myRotation.z);
+        //float turn = Input.GetAxis("Vertical");
+        //rb.AddTorque(transform.up * torque);
+        if(rb.isKinematic == false)
+        {
+            transform.Rotate(Vector3.up * torque);
+        }
 
+        if(startLerping == true)
+        {
+            elapsedTime += Time.deltaTime;
+            torque = Mathf.Lerp(torque, 0, elapsedTime / duration);
+            if (elapsedTime >= duration) startLerping = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        startLerping = true;
+        elapsedTime = 0;
     }
 }
