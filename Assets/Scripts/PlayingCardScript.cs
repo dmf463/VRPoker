e66ac745d1966db18bf298c32018f3ba0b49ddf2
davidFrisch <dmf463@nyu.edu>
2017-05-30 17:21:39 -0400

    got the throw ALMOST there

--- Assets/Scripts/PlayingCardScript.cs
+++ Assets/Scripts/PlayingCardScript.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using Valve.VR; //we need this for SteamVR
 using Valve.VR.InteractionSystem;
 
-public class PlayingCardScript : MonoBehaviour {
+public class PlayingCardScript : InteractionSuperClass {
 
-    Vector3 myRotation;
+    Vector3 throwingRotation;
+    Quaternion myRotation;
     Rigidbody rb;
     public float torque;
     public float duration;
@@ -23,12 +24,14 @@ public class PlayingCardScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        Debug.Log("torque is " + torque);
-        myRotation = transform.eulerAngles;
-        transform.rotation = Quaternion.Euler(0, myRotation.y, myRotation.z);
+        myRotation = transform.rotation;
+        //Debug.Log("torque is " + torque);
+        //throwingRotation = transform.eulerAngles;
+        //transform.rotation = Quaternion.Euler(0, throwingRotation.y, throwingRotation.z);
         if(rb.isKinematic == false)
         {
+            throwingRotation = transform.eulerAngles;
+            transform.rotation = Quaternion.Euler(0, throwingRotation.y, 0);
             transform.Rotate(Vector3.up * (torque * rb.velocity.magnitude));
         }
 
@@ -44,5 +47,36 @@ public class PlayingCardScript : MonoBehaviour {
     {
         startLerping = true;
         elapsedTime = 0;
+    }
+
+    public override void OnTriggerEnterX(Collider other)
+    {
+        base.OnTriggerEnterX(other);
+    }
+
+    public override void OnTriggerExitX(Collider other)
+    {
+        base.OnTriggerExitX(other);
+    }
+
+    public override void HandHoverUpdate(Hand hand)
+    {
+        base.HandHoverUpdate(hand);
+    }
+
+    public override void OnAttachedToHand(Hand attachedHand)
+    {
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+        base.OnAttachedToHand(attachedHand);
+    }
+
+    public override void HandAttachedUpdate(Hand attachedHand)
+    {
+        base.HandAttachedUpdate(attachedHand);
+    }
+
+    public override void OnDetachedFromHand(Hand hand)
+    {
+        base.OnDetachedFromHand(hand);
     }
 }
