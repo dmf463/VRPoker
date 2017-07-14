9ba462946604bba7d6b3e907e849834102643c2b
David Frisch <dmf463@nyu.edu>
2017-07-14 13:33:31 -0400

    fixed oversite where I wasn't clearing the players each round

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -40,6 +40,11 @@ public class GameManager : MonoBehaviour
         player2.SeatPos = Destinations.player2;
         player3.SeatPos = Destinations.player3;
 
+        player0.PlayerState = PlayerState.Playing;
+        player1.PlayerState = PlayerState.Playing;
+        player2.PlayerState = PlayerState.Playing;
+        player3.PlayerState = PlayerState.Playing;
+
         cardsDealt = 0;
 
     }
@@ -75,22 +80,61 @@ public class GameManager : MonoBehaviour
             player2.Hand = player2Hand;
             player3.Hand = player3Hand;
 
-            Debug.Log(player0.SeatPos + " has " + player0.Hand.HandValues.pokerHand + " with a total HandValue of " + player0.Hand.HandValues.Total + " with a highCard of " + player0.Hand.HandValues.HighCard);
-            Debug.Log(player1.SeatPos + " has " + player1.Hand.HandValues.pokerHand + " with a total HandValue of " + player1.Hand.HandValues.Total + " with a highCard of " + player1.Hand.HandValues.HighCard);
-            Debug.Log(player2.SeatPos + " has " + player2.Hand.HandValues.pokerHand + " with a total HandValue of " + player2.Hand.HandValues.Total + " with a highCard of " + player2.Hand.HandValues.HighCard);
-            Debug.Log(player3.SeatPos + " has " + player3.Hand.HandValues.pokerHand + " with a total HandValue of " + player3.Hand.HandValues.Total + " with a highCard of " + player3.Hand.HandValues.HighCard);
+            Debug.Log(player0.SeatPos + " has " + player0.Hand.HandValues.PokerHand + " with a total HandValue of " + player0.Hand.HandValues.Total + " with a highCard of " + player0.Hand.HandValues.HighCard);
+            Debug.Log(player1.SeatPos + " has " + player1.Hand.HandValues.PokerHand + " with a total HandValue of " + player1.Hand.HandValues.Total + " with a highCard of " + player1.Hand.HandValues.HighCard);
+            Debug.Log(player2.SeatPos + " has " + player2.Hand.HandValues.PokerHand + " with a total HandValue of " + player2.Hand.HandValues.Total + " with a highCard of " + player2.Hand.HandValues.HighCard);
+            Debug.Log(player3.SeatPos + " has " + player3.Hand.HandValues.PokerHand + " with a total HandValue of " + player3.Hand.HandValues.Total + " with a highCard of " + player3.Hand.HandValues.HighCard);
 
             players.Add(player0);
             players.Add(player1);
             players.Add(player2);
             players.Add(player3);
 
-            //players.Sort((bestHand, worstHand) => worstHand.Hand.HandValues.pokerHand.CompareTo(bestHand.Hand.HandValues.pokerHand));
-            List<PokerPlayer> sortedPlayers = new List<PokerPlayer>(players.OrderByDescending(bestHand => bestHand.Hand.HandValues.pokerHand).ThenByDescending(bestHand => bestHand.Hand.HandValues.Total).ThenByDescending(bestHand => bestHand.Hand.HandValues.HighCard));
-            Debug.Log(sortedPlayers[0].SeatPos + " has the best hand with a " + sortedPlayers[0].Hand.HandValues.pokerHand + " with a highCard of " + sortedPlayers[0].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[0].Hand.HandValues.Total);
-            Debug.Log(sortedPlayers[1].SeatPos + " has the second best hand with a " + sortedPlayers[1].Hand.HandValues.pokerHand + " with a highCard of " + sortedPlayers[1].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[1].Hand.HandValues.Total);
-            Debug.Log(sortedPlayers[2].SeatPos + " has the third best hand with a " + sortedPlayers[2].Hand.HandValues.pokerHand + " with a highCard of " + sortedPlayers[2].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[2].Hand.HandValues.Total);
-            Debug.Log(sortedPlayers[3].SeatPos + " has the worst hand with a " + sortedPlayers[3].Hand.HandValues.pokerHand + " with a highCard of " + sortedPlayers[3].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[3].Hand.HandValues.Total);
+            List<PokerPlayer> sortedPlayers = new List<PokerPlayer>(players.OrderByDescending(bestHand => bestHand.Hand.HandValues.PokerHand).ThenByDescending(bestHand => bestHand.Hand.HandValues.Total).ThenByDescending(bestHand => bestHand.Hand.HandValues.HighCard));
+            //sortedPlayers[0].PlayerState = PlayerState.Winner;
+
+            //so I COULD do it manually, like:
+            //if(sortedplayers[0].Hand.HandValues.pokerHand == sortedPlayers[1].Hand.HandValues.pokerHand && sortedPlayer[0].Hand.HandValues.pokerHand == sortedPlayers[2].....
+            //BUT THAT WILL TAKE FOREVER
+            //because I need to compare the pokerHand, then the total, then the highCard to see if they're the same or not. 
+            //and doing that for EACH variation of possible tie combinations seems fucking ridiculous
+            //I could make the list Distinct(), but that wouldn't help me with determining a draw. I wish two objects could occupy the same position in a list...
+            //if I can make a new list, that returns duplicates, then compare that list to the first list, and assign winners based on that?
+            /*
+             * so like, if list A has:
+             *                  2, 4, 6, A, A
+             *                  2, 4, 6, A, A
+             *                  2, 2, 6, K, A
+             *                  2, 2, 6, K, A
+             *  that would means that a list of it's duplicates would look like
+             *                  2, 4, 6, A, A
+             *                  2, 2, 6, K, A
+             *  then I can say, if (duplicates[0].Hand.HandValue.PokerHand == ListA[0].Hand.HandValue.PokerHand)
+             *                     {
+             *                          ListA[0].Hand.HandValue.PlayerState == PlayerState.Winner;
+             *                     }
+             *                     AND THEN CHECK THAT ACROSS EVERY SINGLE OTHER PLAYER FOR POKERHAND, TOTAL, AND HIGH CARD. UGH.
+             *                     
+             *What about if I check the first versuse the last. So like, if sortedPlayer[0] != sortedPlayer[3], then I know that they don't ALL have the same hand
+             * but that still leaves me in the same position of having to write out a SHIT TON of logic, just backwards (which if I do, should probably be the case).
+             * the problem is, the amount of players I may have at a given moment might change depending on the state of the game,
+             * so I'm hesitant to hardcose positions in the list, because right now I'm only check for 4 players, but in the game there might be 6, or 10!
+             * 
+             * /*
+             */
+
+
+
+
+
+            Debug.Log(sortedPlayers[0].SeatPos + " has the best hand with a " + sortedPlayers[0].Hand.HandValues.PokerHand + " with a highCard of " + sortedPlayers[0].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[0].Hand.HandValues.Total);
+            Debug.Log(sortedPlayers[1].SeatPos + " has the second best hand with a " + sortedPlayers[1].Hand.HandValues.PokerHand + " with a highCard of " + sortedPlayers[1].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[1].Hand.HandValues.Total);
+            Debug.Log(sortedPlayers[2].SeatPos + " has the third best hand with a " + sortedPlayers[2].Hand.HandValues.PokerHand + " with a highCard of " + sortedPlayers[2].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[2].Hand.HandValues.Total);
+            Debug.Log(sortedPlayers[3].SeatPos + " has the worst hand with a " + sortedPlayers[3].Hand.HandValues.PokerHand + " with a highCard of " + sortedPlayers[3].Hand.HandValues.HighCard + " and a handTotal of " + sortedPlayers[3].Hand.HandValues.Total);
+
+            sortedPlayers.Clear();
+            players.Clear();
+            Debug.Log("sortedPlayers has " + sortedPlayers.Count + "players");
 
         }
 
--- Assets/Scripts/PokerPlayer.cs
+++ Assets/Scripts/PokerPlayer.cs
@@ -2,46 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct PokerPlayer
-{
+public enum PlayerState { Playing, NoHand, Winner, Loser}
+
+
+public class PokerPlayer {
+
     public Destinations SeatPos { get; set; }
     public int ChipCount { get; set; }
-    public List<Card> Cards { get; set;}
     public HandEvaluator Hand { get; set; }
-}
-
-public class PlayerEvaluator {
-
-    private PokerPlayer pokerPlayer;
-
-    public PokerPlayer PokerPlayer
-    {
-        get { return pokerPlayer; }
-        set { pokerPlayer = value; }
-    }
-
-    public void GetPlayerInfo(Destinations dest, HandEvaluator hand)
-    {
-        if(dest == Destinations.player0)
-        {
-            pokerPlayer.SeatPos = Destinations.player0;
-            pokerPlayer.Hand = hand;
-        }
-        else if (dest == Destinations.player1)
-        {
-            pokerPlayer.SeatPos = Destinations.player1;
-            pokerPlayer.Hand = hand;
-        }
-        else if (dest == Destinations.player2)
-        {
-            pokerPlayer.SeatPos = Destinations.player2;
-            pokerPlayer.Hand = hand;
-        }
-        else if (dest == Destinations.player3)
-        {
-            pokerPlayer.SeatPos = Destinations.player3;
-            pokerPlayer.Hand = hand;
-        }
-    }
+    public PlayerState PlayerState { get; set; }
 
 }
+
