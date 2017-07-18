4fb58b71bf6d317f8580670fe310b4913edf59e6
David Frisch <dmf463@nyu.edu>
2017-07-18 12:33:38 -0400

    started adding in chips

--- Assets/Scripts/PrefabDB.cs
+++ Assets/Scripts/PrefabDB.cs
@@ -13,4 +13,20 @@ public class PrefabDB : ScriptableObject {
     private GameObject card;
     public GameObject Card { get { return card; } }
 
+    [SerializeField]
+    private GameObject redChip5;
+    public GameObject RedChip5 { get { return redChip5; } }
+
+    [SerializeField]
+    private GameObject blueChip25;
+    public GameObject BlueChip25 { get { return blueChip25; } }
+
+    [SerializeField]
+    private GameObject whiteChip50;
+    public GameObject WhiteChip50 { get { return whiteChip50; } }
+
+    [SerializeField]
+    private GameObject blackChip100;
+    public GameObject BlackChip100 { get { return blackChip100; } }
+
 }
