be9957c66b31262f948adccbf7df04f54ab30979
Unknown <longlegsclassic@gmail.com>
2017-11-14 15:28:45 -0500

    Added raycast to cards

---
+++ Assets/Scripts/CardRaycast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardRaycast : MonoBehaviour {
+
+	float timeRaycastIsHitting = 0f;
+	public float rayDistance = 100f;
+
+	public LayerMask mask;
+
+
+	// Update is called once per frame
+	void Update () {
+		Ray ray = new Ray (transform.position, -transform.forward);
+
+		RaycastHit rayHit;
+
+		if(Physics.Raycast(ray, out rayHit, rayDistance, mask))
+		{
+			//print(rayHit.collider.gameObject.name);
+			Debug.DrawLine(ray.origin, rayHit.point, Color.green);
+		}else{
+			Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.red);
+		}
+	}
+}
