e630c7d8f52015e8798c4647de37142b58bba85c
Unknown <dmf463@nyu.edu>
2017-11-20 14:45:02 -0500

    got card indicator updgraded, need to place them

---
+++ Assets/Scripts/CardIndicatorLerp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardIndicatorLerp : MonoBehaviour {
+
+    public float lerpDistance;
+    public float lerpSpeed;
+    private float yPos;
+	// Use this for initialization
+	void Start () {
+        yPos = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, yPos + Mathf.PingPong(Time.time / lerpSpeed, lerpDistance), transform.position.z); 
+		
+	}
+}
