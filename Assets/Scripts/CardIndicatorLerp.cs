b46753da80b50ff1aef6982fef843adfc39a89c6
Unknown <dmf463@nyu.edu>
2018-01-30 18:40:53 -0500

    setting up for cheating

--- Assets/Scripts/CardIndicatorLerp.cs
+++ Assets/Scripts/CardIndicatorLerp.cs
@@ -7,6 +7,7 @@ public class CardIndicatorLerp : MonoBehaviour {
     public float lerpDistance;
     public float lerpSpeed;
     private float yPos;
+    public float yOffset;
 	// Use this for initialization
 	void Start () {
         yPos = transform.position.y;
@@ -14,7 +15,14 @@ public class CardIndicatorLerp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
         transform.position = new Vector3(transform.position.x, yPos + Mathf.PingPong(Time.time / lerpSpeed, lerpDistance), transform.position.z); 
 		
+        if(gameObject.tag == "CardPreview")
+        {
+            transform.position = new Vector3(transform.position.x, 
+                                             transform.parent.position.y + yOffset + Mathf.PingPong(Time.time / lerpSpeed, lerpDistance), 
+                                             transform.position.z);
+        }
 	}
 }
