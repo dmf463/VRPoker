0ec9f750a168cebd20df5275841fc4032d223025
Unknown <dmf463@nyu.edu>
2018-02-10 19:36:33 -0500

    new indicators for where each card goes

---
+++ Assets/Scripts/GlowLerp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpriteGlow;
+
+public class GlowLerp : MonoBehaviour {
+
+    float glowMax = 1.5f;
+    float glowMin = 1;
+    float currentGlow = 0;
+    public float glowSpeed = 1;
+    SpriteGlowEffect glowScript;
+
+	// Use this for initialization
+	void Start () {
+
+        glowScript = GetComponent<SpriteGlowEffect>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        glowScript.GlowBrightness = PingPong(glowSpeed * Time.time, glowMin, glowMax);
+
+    }
+
+    float PingPong(float time, float minLength, float maxLength)
+    {
+        return Mathf.PingPong(time, maxLength - minLength) + minLength;
+    }
+}
