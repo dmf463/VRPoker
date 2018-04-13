29522f490a8257191a71316191bf99c5d9bc8987
Unknown <dmf463@nyu.edu>
2018-04-13 18:53:27 -0400

    pushing a horrifying opening

---
+++ Assets/Scripts/FlickeringLights.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLights : MonoBehaviour
+{
+    public List<GameObject> lights;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject obj in lights)
+        {
+            if (obj.activeSelf == true)
+            {
+                if (Random.Range(0, 100) < 5) obj.SetActive(false);
+            }
+            else if (obj.activeSelf == false)
+            {
+                if (Random.Range(0, 100) < 5) obj.SetActive(true);
+            }
+
+        }
+    }
+}
